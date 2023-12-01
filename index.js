e1262b0c7e7167509dccdf75418fb5bcba441f7d
Dione <Dione783@hotmail.com>
2023-11-30 23:20:37 -0300

    Ajuste

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-let elos = ["Ferro","Bronze","Prata","Ouro","Platina","Ascendente","Imortal","Radiante"]
+let elos = {Ferro:"Ferro",Bronze:"Bronze",Prata:"Prata",Ouro:"Ouro",Platina:"Platina",Ascendente:"Ascendente",Imortal:"Imortal",Radiante:"Radiante"}
 let heroi = {
     xp:10.001,
     elo:""
 }
     if(heroi.xp >= 10.000){
-        heroi.elo="Radiante"
+        heroi.elo=elos.Radiante
     }
     if(heroi.xp < 10.001){
-        heroi.elo="imortal"
+        heroi.elo=elos.Imortal
     }
     if(heroi.xp < 9.001){
-        heroi.elo="Ascendente"
+        heroi.elo=elos.Ascendente
     }
     if(heroi.xp < 8.001){
-        heroi.elo="Platina"
+        heroi.elo=elos.Platina
     }
     if(heroi.xp < 7.001){
-        heroi.elo="Ouro"
+        heroi.elo=elos.Ouro
     }
     if(heroi.xp < 5.001){
-        heroi.elo="Prata"
+        heroi.elo=elos.Prata
     }
     if(heroi.xp < 2.001){
-        heroi.elo="Bronze"
+        heroi.elo=elos.Bronze
     }
     if(heroi.xp < 1.001){
-        heroi.elo="Ferro"
+        heroi.elo=elos.Ferro
     }
 console.log(heroi.elo)
